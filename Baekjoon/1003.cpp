5702a7a0db5486253f2f2543963e78f470f92592
Hyein Kim <kimhyein7110@gmail.com>
2021-02-08 23:56:36 +0900

    210208

--- Baekjoon/1003.cpp
+++ Baekjoon/1003.cpp
@@ -1,34 +1,21 @@
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
-int cnt0, cnt1;
-
-int fib(int n) {
-	if (n == 0) {
-		cnt0++;
-		return 0;
-	}
-	else if (n == 1) {
-		cnt1++;
-		return 1;
-	}
-	else {
-		return fib(n - 1) + fib(n - 2);
-	}
-}
+int fibo[40];
 
 int main() {
-	int t;
-	scanf("%d", &t);
+	fibo[0] = 0;
+	fibo[1] = 1;
 
+	for (int i = 2; i < 41; i++) fibo[i] = fibo[i - 1] + fibo[i - 2];
+
+	int t; 
+	cin >> t;
 	for (int i = 0; i < t; i++) {
-		cnt0 = 0;
-		cnt1 = 0;
-		int x;
-		scanf("%d", &x);
-		int result = fib(x);
-		printf("%d %d\n", cnt0, cnt1);
+		int n;
+		cin >> n;
+		if (n == 0) cout << "1 0" << endl;
+		else if (n == 1) cout << "0 1" << endl;
+		else cout << fibo[n - 1] << " " << fibo[n] << endl;
 	}
-
-	return 0;
 }
