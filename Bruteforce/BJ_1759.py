2d165bff908e6dd76184ea5f749e44cf502bc833
hyein <kimhyein7110@gmail.com>
2021-09-03 23:52:51 +0900

    210903

--- Bruteforce/BJ_1759.py
+++ Bruteforce/BJ_1759.py
@@ -1,37 +1,15 @@
+from itertools import combinations
+
+vowels = ('a', 'e', 'i', 'o', 'u')
 l, c = map(int, input().split())
-cipher = list(input().split())
-visited = [False] * c
-vowels = ['a', 'e', 'i', 'o', 'u']
-cipher.sort()
 
-string =""
+array = input().split()
+array.sort()
 
-def BackTrack(depth, start):
-    global string
-    if depth == l:
-        Vaildation(string)
-        return
-
-    for i in range(start, c):
-        if visited[i]:
-            continue
-        visited[i] = True
-        string += cipher[i]
-        BackTrack(depth+1, i)
-        string = string[:-1]
-        visited[i] = False
-
-def Vaildation(string):
-    vowel_count = 0
-    consonant_count = 0
-    for i in range(len(string)):
-        if string[i] in vowels:
-            vowel_count += 1
-        else:
-            consonant_count += 1
-
-        if i == len(string) - 1:
-            if vowel_count >= 1 and consonant_count >= 2 :
-                print(string)
-
-BackTrack(0, 0)
+for password in combinations(array, l):
+  count = 0
+  for i in password:
+    if i in vowels:
+      count += 1
+  if count >= 1 and count <= l - 2:
+    print(''.join(password))
