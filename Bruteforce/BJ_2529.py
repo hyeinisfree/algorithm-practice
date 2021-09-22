30ee9cb546de0fecb9fd2c1c1ecccbe1f362a32d
hyein <kimhyein7110@gmail.com>
2021-09-22 23:48:36 +0900

    210922

--- Bruteforce/BJ_2529.py
+++ Bruteforce/BJ_2529.py
@@ -1,12 +1,12 @@
 import sys
-import itertools
 input = sys.stdin.readline
 
 k = int(input())
 sign = input().split()
 check = [False] * 10
-max , min = "",""
-def poss(i,j,k): # 부등호 조건이  만족할 때만 작동
+max, min = "", ""
+
+def poss(i,j,k):
   if k == ">":
     return i>j
   else:
@@ -15,19 +15,19 @@ def poss(i,j,k): # 부등호 조건이  만족할 때만 작동
 
 def recu(cnt, s):
   global max,min
-  if cnt > k: #맨처음 나타나는 값이 최소, 마지막 저장되는 것이 최대
+  if cnt > k:
     if len(min) == 0:
       min = s
     else:
       max = s
     return
-  for i in range(10): #재귀 함수
+  for i in range(10):
     if check[i] == False:
-      if cnt == 0 or poss(s[-1],str(i),sign[cnt-1]):
+      if cnt == 0 or poss(s[-1], str(i), sign[cnt-1]):
         check[i] = True
         recu(cnt+1,s+str(i))
         check[i] = False
 
-recu(0,"")
+recu(0, "")
 print(max)
 print(min)
