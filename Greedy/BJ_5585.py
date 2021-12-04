35618eb0db1ff9352f67cd4e70ca1222bf9535ba
hyeinisfree <kimhyein7110@gmail.com>
2021-12-04 17:56:05 +0900

    211204

---
+++ Greedy/BJ_5585.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+money = int(input())
+n = 1000 - money
+
+count = 0
+coin = [500, 100, 50, 10, 5, 1]
+  
+for i in coin:
+  count += n // i
+  n %= i
+
+print(count)
