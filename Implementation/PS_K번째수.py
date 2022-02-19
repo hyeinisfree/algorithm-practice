48e027ef3972e5cfc400acbfcb7f37195b3070da
hyeinisfree <kimhyein7110@gmail.com>
2022-02-19 19:16:30 +0900

    220219

--- Implementation/PS_K번째수.py
+++ Implementation/PS_K번째수.py
@@ -1,5 +1,11 @@
+def solution1(array, commands):
+    answer = []
+    for c in commands:
+        i, j, k = c
+        answer.append(sorted(array[i-1:j])[k-1])
+    return answer
 
-def solution(array, commands):
+def solution2(array, commands):
     answer = []
     for c in commands:
         i, j, k = c
@@ -7,10 +13,3 @@ def solution(array, commands):
         new.sort()
         answer.append(new[k-1])
     return answer
-
-# def solution(array, commands):
-#     answer = []
-#     for command in commands:
-#         i,j,k = command
-#         answer.append(list(sorted(array[i-1:j]))[k-1])
-#     return answer
--- Implementation/PS_소수만들기.py
+++ Implementation/PS_소수만들기.py
@@ -3,15 +3,17 @@ import math
 
 def solution(nums):
     answer = 0
-    data = list(combinations(nums, 3))
-    for i in data:
-        n = sum(i)
-        if is_prime(n):
-            answer += 1
-    return answer
+    comb = list(combinations(nums, 3))
     
-def is_prime(x):
-    for i in range(2, int(math.sqrt(x)) + 1):
-        if x % i == 0:
+    for c in comb:
+        if is_prime(sum(c)):
+            answer += 1
+
+    return answer
+
+def is_prime(num):
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
             return False
+          
     return True
