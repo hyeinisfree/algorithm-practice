2f7005061c97343a93e614a5ed6ad938e9d5e55e
hyeinisfree <kimhyein7110@gmail.com>
2022-03-04 00:34:36 +0900

    220304

---
+++ Backtracking/LC_17.py
@@ -0,0 +1,20 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        keypad = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        output = []
+        
+        if len(digits) == 0:
+            return []
+
+        def BT(index, letter):
+            if index == len(digits):
+                output.append(letter)
+                return
+
+            num = int(digits[index])
+            chars = keypad[num]
+            for c in chars:
+                BT(index+1, letter+c)
+        
+        BT(0, '')
+        return output
---
+++ Backtracking/LC_78.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+    
+        def BT(index, crnt_set):
+            if index == len(nums):
+                output.append(crnt_set.copy())
+                return
+
+            num = nums[index]
+            BT(index+1, crnt_set)
+
+            crnt_set.append(num)
+            BT(index+1, crnt_set)
+            crnt_set.pop()
+
+        BT(0, [])
+        return output  
