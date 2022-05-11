cf3fdbbc26b8fab714f613e952c6d26b7cc779c6
hyeinisfree <kimhyein7110@gmail.com>
2022-05-11 23:24:59 +0900

    220511

--- Backtracking/LC_17.py
+++ Backtracking/LC_17.py
@@ -2,21 +2,23 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        keypad = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-        output = []
-        
+        self.keypad = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+    
         if len(digits) == 0:
             return []
-
-        def BT(index, letter):
-            if index == len(digits):
-                output.append(letter)
-                return
-
-            num = int(digits[index])
-            chars = keypad[num]
-            for c in chars:
-                BT(index+1, letter+c)
+    
+        self.digits = digits = digits
+        self.comb = []
+        self.bt(0, '')
         
-        BT(0, '')
-        return output
+        return self.comb
+    
+    def bt(self, index: int, crntStr: str):
+        if index == len(self.digits):
+            self.comb.append(crntStr)
+            return
+        
+        num = int(self.digits[index])
+        chars = self.keypad[num]
+        for char in chars:
+            self.bt(index+1, crntStr+char)
