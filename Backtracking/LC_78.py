d907122c9de40aebc1054b76096704bb99c37fba
hyeinisfree <kimhyein7110@gmail.com>
2022-05-12 23:28:51 +0900

    220512

--- Backtracking/LC_78.py
+++ Backtracking/LC_78.py
@@ -2,19 +2,21 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = []
+        self.nums = nums
+        self.subsets = []
+        
+        self.bt(0, [])
+        
+        return self.subsets
     
-        def BT(index, crnt_set):
-            if index == len(nums):
-                output.append(crnt_set.copy())
-                return
+    def bt(self, index: int, crnt_set: List[int]):
+        if index == len(self.nums):
+            self.subsets.append(crnt_set.copy())
+            return
 
-            num = nums[index]
-            BT(index+1, crnt_set)
+        num = self.nums[index]
+        self.bt(index+1, crnt_set)
 
-            crnt_set.append(num)
-            BT(index+1, crnt_set)
-            crnt_set.pop()
-
-        BT(0, [])
-        return output  
+        crnt_set.append(num)
+        self.bt(index+1, crnt_set)
+        crnt_set.pop()
