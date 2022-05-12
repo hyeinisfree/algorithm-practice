0e246b99691bcf9b5dc8d89db117fc0db7f261d4
hyeinisfree <kimhyein7110@gmail.com>
2022-05-13 00:33:40 +0900

    220513

--- Backtracking/LC_78.py
+++ Backtracking/LC_78.py
@@ -20,3 +20,22 @@ class Solution:
         crnt_set.append(num)
         self.bt(index+1, crnt_set)
         crnt_set.pop()
+
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
+        stack = []
+        stack.append((0, []))
+        while stack:
+            index, crnt_set = stack.pop()
+            if index == len(nums):
+                subsets.append(crnt_set)
+                continue
+            
+            num = nums[index]
+            stack.append((index+1, crnt_set.copy()))
+            
+            crnt_set.append(num)
+            stack.append((index+1, crnt_set.copy()))
+        
+        return subsets
